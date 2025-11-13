86d67f3056e895f7091d9885120cb60f5aa1aa4d
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2025-11-13 01:13:03 -0300

    v2

--- backend/settings.py
+++ backend/settings.py
@@ -149,3 +149,9 @@ if config('PA_USE_SQLITE', default=False, cast=bool):
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
